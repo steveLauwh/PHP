c6ec864442391920432f0f0a1f055f317d6ad838
Steve Lau <liuwenhong_steve@163.com>
2018-03-01 00:26:31 +0800

    Update user.php

--- PHP 实例或小项目/steveLau.com/lib/user.php
+++ PHP 实例或小项目/steveLau.com/lib/user.php
@@ -1,16 +1,22 @@
 <?php
 
+// 加载文件一次
+// __DIR__ 指向当前执行的 PHP 脚本所在的目录
 require_once __DIR__.'/errorCode.php';
 
+// 定义 User 类，实现用户注册和登录
 class User
 {
-	private $_db;
+	private $_db; // 私有变量
 
+	// 构造函数
 	public function __construct($_db)
 	{
+		// $this 代表自身的对象
 		$this->_db = $_db;
 	}
 
+	// 登录功能
 	public function login($username, $password)
 	{
 		if (empty($username))
@@ -43,18 +49,22 @@ class User
 		return $user;
 	}
 
+	// 注册功能
 	public function register($username, $password)
 	{
+		// empty-检查用户名是否为空
 		if (empty($username))
 		{
 			throw new Exception('Username is not empty', ErrorCode::USERNAME_CANNOT_EMPTY);
 		}
 
+		// empty-检查密码是否为空
 		if (empty($password))
 		{
 			throw new Exception('Password is not empty', ErrorCode::PASSWORD_CANNOT_EMPTY);
 		}
 
+		// 检查用户名是否存在
 		if ($this->_isUsernameExists($username)) 
 		{
 			throw new Exception('Username is Exist', ErrorCode::USERNAME_EXISTS);
@@ -88,15 +98,16 @@ class User
 		return md5($string . $key);
 	}
 
+	// 检查用户名是否存在
 	private function _isUsernameExists($username)
 	{
 		$sql = 'SELECT * FROM `user` WHERE `username`=:username';
-		$stmt = $this->_db->prepare($sql);
-		$stmt->bindParam(':username', $username);
-		$stmt->execute();
-		$result = $stmt->fetch(PDO::FETCH_ASSOC);
+		$stmt = $this->_db->prepare($sql); // PDO::prepare 准备要执行 SQL 语句
+		$stmt->bindParam(':username', $username); // PDOStatement::bindParam 绑定一个参数(:name 形式的参数名)到指定的变量名
+		$stmt->execute(); // PDOStatement::execute 执行预处理语句
+		$result = $stmt->fetch(PDO::FETCH_ASSOC); // 返回一个索引为结果集列名的数组
 
-		return !empty($result);
+		return !empty($result); 
 	}
 }
 ?>
