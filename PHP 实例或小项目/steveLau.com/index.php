f7bdb67a66c9c12b8c25ec07cd2e24b2abc2b208
Steve Lau <liuwenhong_steve@163.com>
2018-02-28 22:28:40 +0800

    验证 User 和 Article 逻辑

--- PHP 实例或小项目/steveLau.com/index.php
+++ PHP 实例或小项目/steveLau.com/index.php
@@ -6,18 +6,20 @@ require __DIR__.'/lib/article.php';
 $pdo = require __DIR__.'/lib/db.php';
 
 $user = new User($pdo);
-//print_r($user->register('Lau', 'steve'));
 
-//print_r($user->login('Lau', 'steve'));
+print_r($user->register('Lau', 'steve'));
+
+print_r($user->login('Lau', 'steve'));
 
 $article = new Article($pdo);
-//print_r($article->create('article', 'content', 2));
 
-//print_r($article->view(8));
+print_r($article->create('article', 'content', 2));
 
-//print_r($article->edit(8, 'article2', 'content_new', 2));
+print_r($article->view(8));
 
-//var_dump($article->delete(2, 1));
+print_r($article->edit(8, 'article2', 'content_new', 2));
+
+var_dump($article->delete(2, 1));
 
 print_r($article->getlist(1, 1, 4));
 
