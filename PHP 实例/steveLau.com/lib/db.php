ae049c932177b7d162663eedabf8a7cf7420f1b5
Steve Lau <liuwenhong_steve@163.com>
2018-02-28 17:14:31 +0800

    使用 PDO 连接 MySQL 数据库

--- PHP 实例/steveLau.com/lib/db.php
+++ PHP 实例/steveLau.com/lib/db.php
@@ -1,17 +1,18 @@
 <?php
 /**
- * 连接数据库并返回数据库连接句柄
+ * 使用 PDO 连接数据库并返回数据库连接句柄
  */
-$dbms='mysql';
-$host='localhost';
-$dbName='mydb';
-$user='root';
-$pass='123456';
+$dbms='mysql'; 	     // 数据库类型
+$host='localhost';   // 数据库主机名
+$dbName='mydb';      // 使用的数据库
+$user='root';        // 数据库连接的用户名
+$pass='123456';      // 对应的密码
 $dsn="$dbms:host=$host;dbname=$dbName";
 
 try {
-	$pdo = new PDO($dsn, $user, $pass);
-	$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+	$pdo = new PDO($dsn, $user, $pass);  // 初始化一个 PDO 对象
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // 设置捕获异常
+	$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); // 设置禁止本地模拟 prepare
 	echo "连接成功<br/>";
 	return $pdo;
 } catch (PDOException $e) {
